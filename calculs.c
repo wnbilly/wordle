fa9a589e3d96fa3dacbbc47ec34f4937d18c29fb
wnbilly <weynansbilly@gmail.com>
2022-04-28 17:19:40 +0200

    ca avance

--- calculs.c
+++ calculs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "acquisition.h"
 #include "tests_lettres.h"
 #include "calculs.h"
@@ -96,7 +97,7 @@ int test_ltr_ban(char* mot_test, struct donnees* data)
 void extraction_donnees(char* essai, int resultat[], struct donnees* data)
 {
     int nb_lettres = strlen(essai);
-    int indice_ban = 0;
+    //int indice_ban = 0;
 
     for (int i=0; i<nb_lettres; i++) //i indice resultat
     {
@@ -150,7 +151,7 @@ void affichage_donnees(struct donnees* data)
     printf("\n");
 }
 
-//
+//retourne la liste de mots compatibles avec les données parmi les mots_a_tester
 int liste_mots_prob(char* mots_probables[], char* mots_a_tester[], int nb_mots, struct donnees* data)
 {
     int indice_mot_prob = 0;
@@ -166,22 +167,20 @@ int liste_mots_prob(char* mots_probables[], char* mots_a_tester[], int nb_mots, 
     return indice_mot_prob-1;
 }
 
-int nb_mots_prob(char* mot, int** liste_patterns, char* mots_a_tester[])
+//Retourne le nombre de mots compatibles avec mot+pattern
+//A REVOIR
+int nombre_mots_prob(char* mot, int pattern[], char* mots_a_tester[], int nb_mots_test)
 {
     int nb_prob = 0;
-    int nb_lettres = sizeof(mot)/sizeof(char);
-    int nb_mots_test = sizeof(mots_a_tester)/sizeof(mot);
+    //int nb_lettres = 5;
     for (int i=0; i<nb_mots_test; i++)
     {
         char* mot_test = mots_a_tester[i];
-        for (int p =0; p<243; p++)
+        struct donnees* data = malloc(sizeof(struct donnees));
+        extraction_donnees(mot_test, pattern, data);
+        if (correspondance_ltr_jaune(mot_test, data)==1 && correspondance_ltr_verte(mot_test, data)==1 && test_ltr_ban(mot_test, data)==1)
         {
-            struct donnees* data;
-            extraction_donnees(mot_test, liste_patterns[p], data);
-            if (correspondance_ltr_jaune(mot_test, data)==1 && correspondance_ltr_verte(mot_test, data)==1 && test_ltr_ban(mot_test, data)==1)
-            {
-                nb_prob++;
-            }
+            nb_prob++;
         }
     }
     return nb_prob;
@@ -218,16 +217,36 @@ int** creation_liste_patterns(int nb_lettres) //OU BASE 3
 }
 
 
-int calcul_entropie(char* mot, int** liste_patterns, int nb_mots)
+int calcul_entropie_mot(char* mot, int nb_mots, char* mots_a_tester[])
 {
+    int nb_lettres = 5;
+    int** liste_patterns = creation_liste_patterns(nb_lettres);
     int h = 0;
     int p = 0;
-    for (int i = 0; i<243; i++);
-        char* mots_probables[];
-        p = liste_mots_prob(mots_probables, mots_a_tester, nb_mots)
-
+    int j;
+    for (j = 0; j<243; j++);
+    {
+        int pattern[] = liste_patterns[j];
+        p = nombre_mots_prob(mot, pattern, mots_a_tester, nb_mots)/nb_mots;
+        h = h+p*log(p);
+    }
 }
 
+int trouver_mot_h_max(char* mots_a_tester[], struct donnees* data, char mot_h_max[])
+{
+    int nb_mots_a_tester = sizeof(mots_a_tester)/sizeof(mots_a_tester[0]);
+    int h_max = 0;
+    for (int k=0; k<nb_mots_a_tester; k++)
+    {
+        int h_temp = calcul_entropie_mot(mots_a_tester[k],nb_mots_a_tester, mots_a_tester);
+        if (h_temp>h_max)
+        {
+            h_max = h_temp;
+            mot_h_max = mots_a_tester[k];
+        }
+    }
+    return h_max;
+}
 
 int main(int argc, char* argv[])
 {
@@ -251,11 +270,17 @@ int main(int argc, char* argv[])
 
     char* nom_fichier = "liste_complete_triee.txt";
 
-    int taille_test = 6000;
+    int taille_test = 7000;
     char* mots[taille_test];
     char* mots_probables[taille_test];
+    int nb_mots_test = sizeof(mots)/sizeof(essai);
+    char mot_h_max[5];
 
     int nb_mots = extraction_mots(mots, nom_fichier, data->nb_lettres);
+
+    trouver_mot_h_max(mots, data, mot_h_max);
+
+    print("mot_hç_max est %s", mot_h_max);
 
     int nb_mots_probables = liste_mots_prob(mots_probables, mots, nb_mots, data);
 
